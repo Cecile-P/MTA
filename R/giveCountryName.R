1c008e014953559cf0fcf82765d4bda55cb36fd6
Cecile-P <cecile.pizot@i-pri.org>
2020-02-07 15:34:01 +0100

    test

---
+++ R/giveCountryName.R
@@ -0,0 +1,11 @@
+#Function that gives the name of the corresponding country code
+#tabCountryCode: table with two columns, one with the names of countries and one with the corresponding country code
+#code: vector containing country codes
+giveCountryName = function(tabCountryCode,code){
+  countryName = c()
+  for(co in code){
+    name = as.vector(tabCountryCode[which(tabCountryCode$country==co),"name"])
+    countryName = c(countryName,name)
+  }
+  return(countryName)
+}
